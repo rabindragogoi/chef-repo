7753a49ee54ab4cd03b380d5505a891b5521bd58
root <root@node1.mylab.local>
2018-08-22 18:55:03 +0000

    added a cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,5 +1,36 @@
 #
-# Cookbook:: apache
+# Cookbook:: test
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+user 'web_admin' do
+ comment 'A web_admin user to check the cookbook'
+end
+
+
+directory '/var/www/customers/public_html/' do
+  recursive true
+end
+
+file '/var/www/customers/public_html/index.php' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode '0755'
+  owner 'web_admin'
+  group 'web_admin'
+end
+
+yum_package 'git'
+
+package "httpd" do
+	action :install
+end
+
+service "httpd" do
+	action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+	mode "0644"
+end
